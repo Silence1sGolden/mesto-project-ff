6dd4345a1cd87fb2872f635b55901c79d71ebd1f
Osiptsov Danil <posikunets999@gmail.com>
2024-08-05 16:34:13 +0300

    создание валидации

---
+++ src/components/validation.js
@@ -0,0 +1,83 @@
+function enableValidation(data) {
+    const {
+        formSelector,
+        inputSelector,
+        submitButtonSelector,
+        inactiveButtonClass,
+        inputErrorClass,
+        errorClass
+    } = data;
+    const forms = Array.from(document.querySelectorAll(formSelector));
+
+    forms.forEach((form) => {
+        const inputList = Array.from(form.querySelectorAll(inputSelector))
+
+        inputList.forEach((input) => {
+            input.addEventListener('input', () => {
+                isValid(form, input, inputErrorClass, errorClass);
+                toggleButtonState(inputList, form.querySelector(submitButtonSelector), inactiveButtonClass);
+            });
+        })
+    })
+}
+
+function isValid(form, input, inputErrorClass, errorClass) {
+    const errorElement = form.querySelector(`.${input.id}-error`);
+    if (input.validity.patternMismatch) {
+        showInputError(input, inputErrorClass, errorClass, input.dataset.errorMsg, errorElement);
+    } else {
+        if (input.validity.valid) {
+            hideInputError(input, inputErrorClass, errorClass, errorElement)
+        } else {
+            showInputError(input, inputErrorClass, errorClass, input.validationMessage, errorElement);
+        }
+    }
+}
+
+function toggleButtonState(inputList, buttonElement, inactiveButtonClass) {
+    if (hasInvalidInput(inputList)) {
+        buttonElement.disabled = true;
+        buttonElement.classList.add(inactiveButtonClass);
+      } else {
+        buttonElement.disabled = false;
+        buttonElement.classList.remove(inactiveButtonClass);
+      }
+}
+
+function clearValidation(form) {
+    const inputElements = Array.from(form.querySelectorAll('.popup__form'));
+    const errorElements = Array.from(form.querySelectorAll('.popup__error'));
+    const button = form.querySelector('.popup__button');
+    inputElements.forEach((elem) => {
+        elem.classList.remove('popup__input_type_error');
+    })
+    errorElements.forEach((elem) => {
+        elem.classList.remove('popup__error_visible');
+        elem.textContent = '';
+    })
+    button.disabled = true;
+    button.classList.add('popup__button_disabled');
+}
+
+function showInputError(input, inputErrorClass, errorClass, errorMessage, errorElement) {
+    input.classList.add(inputErrorClass);
+    errorElement.classList.add(errorClass);
+    errorElement.textContent = errorMessage;
+}
+
+function hideInputError(input, inputErrorClass, errorClass, errorElement) {
+    input.classList.remove(inputErrorClass);
+    errorElement.classList.remove(errorClass);
+    errorElement.textContent = '';
+}
+
+function hasInvalidInput(inputList) {
+    return inputList.some((item) => {
+        return !item.validity.valid;
+    })
+}
+
+export {
+    enableValidation,
+    clearValidation
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./pages/index.css";
 import { initialCards } from "./components/cards.js";
 import { createNewCard, handleLikeButton, deleteCard } from "./components/card.js";
 import { openModal, closeModal } from "./components/modal.js";
+import { enableValidation, clearValidation } from "./components/validation.js";
 
 const cardList = document.querySelector(".places__list");
 const cardAddButton = document.querySelector(".profile__add-button");
@@ -16,6 +17,15 @@ const popupEditForm = document.forms['edit-profile'];
 const popupTypeNewCard = document.querySelector(".popup_type_new-card");
 const popupNewCardForm = document.forms['new-place'];
 
+enableValidation({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+});
+
 initialCards.forEach((item) =>
   cardList.append(createNewCard(item.name, item.link, deleteCard, openCardPopup, handleLikeButton))
 );
@@ -23,11 +33,12 @@ initialCards.forEach((item) =>
 profileEditButton.addEventListener("click", () => {
   popupEditForm.name.value = profileName.textContent;
   popupEditForm.description.value = profileDescription.textContent;
-  
+  clearValidation(popupEditForm);
   openModal(popupTypeEdit);
 });
 
 cardAddButton.addEventListener("click", () => {
+  clearValidation(popupNewCardForm);
   openModal(popupTypeNewCard);
 });
 
@@ -51,25 +62,21 @@ function openCardPopup(evt) {
 function handleProfileEditFormSubmit(evt) {
   evt.preventDefault();
 
-  if (evt.target.closest('.popup').classList.contains('popup_type_edit')) {
-    profileName.textContent = popupEditForm.name.value;
-    profileDescription.textContent = popupEditForm.description.value;
+  profileName.textContent = popupEditForm.name.value;
+  profileDescription.textContent = popupEditForm.description.value;
 
-    closeModal(evt.target.closest(".popup"));
-  }
+  closeModal(evt.target.closest(".popup"));
 }
 
 function handleNewCardFormSubmit(evt) {
   evt.preventDefault();
   
-  if (evt.target.closest('.popup').classList.contains('popup_type_new-card')) {
-    const newCardName = popupNewCardForm['place-name'].value;
-    const newCardURL = popupNewCardForm.link.value;
-    const newCard = createNewCard(newCardName, newCardURL, deleteCard, openCardPopup, handleLikeButton);
-    
-    cardList.prepend(newCard);
-    popupNewCardForm.reset();
+  const newCardName = popupNewCardForm['place-name'].value;
+  const newCardURL = popupNewCardForm.link.value;
+  const newCard = createNewCard(newCardName, newCardURL, deleteCard, openCardPopup, handleLikeButton);
   
-    closeModal(evt.target.closest(".popup"));
-  }
+  cardList.prepend(newCard);
+  popupNewCardForm.reset();
+
+  closeModal(evt.target.closest(".popup"));
 }
